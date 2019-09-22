9a31c6eafee51fdb54d8daff6582aeecd08973b8
Todd Olsen <todd.olsen@me.com>
2019-09-22 14:31:18 -0400

    initializer

--- Sources/Word/Word.swift
+++ Sources/Word/Word.swift
@@ -2,8 +2,18 @@ public struct Word: Codable {
     public let text: String
     public let frequency: Double?
     public let syllables: [Syllable]
-    public let pronunciation: [Ipa]
+    public let pronunciations: [Ipa]
     public let definitions: [Definition]
+}
+
+public extension Word {
+    init(text: String, syllables: [Syllable], pronunciations: [Ipa]) {
+        self.text = text
+        self.frequency = nil
+        self.syllables = syllables
+        self.pronunciations = pronunciations
+        self.definitions = []
+    }
 }
 
 public struct Definition: Codable {
